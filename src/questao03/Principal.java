91485e23f5317955b06a28cb6948d8708f8c7bec
Alixandre <alixandrealmeida137@gmail.com>
2025-08-02 20:26:17 -0300

    resolucao da questao03

---
+++ src/questao03/Principal.java
@@ -0,0 +1,17 @@
+package questao03;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int x;
+    System.out.println("Digite um número: ");
+    x = entrada.nextInt();
+
+    if (x % 2 == 0){
+        System.out.println("O número digitado é par.");
+    } else {
+        System.out.println("O número digitado é ímpar.");
+    }
+    }
+}
