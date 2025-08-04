a2b0efd43ff25fbaca17aee42612b6626372f708
Alixandre <alixandrealmeida137@gmail.com>
2025-08-04 19:38:34 -0300

    resolucao da questao09

---
+++ src/questao09/Principal.java
@@ -0,0 +1,12 @@
+package questao09;
+
+public class Principal {
+    public static void main (String [] args){
+
+        int i = 0;
+        while (i <= 50){
+            System.out.print(-i);
+            i++;
+        }
+    }
+}
