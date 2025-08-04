8278211f42517db12298a542d3e08c402c35856f
Alixandre <alixandrealmeida137@gmail.com>
2025-08-04 19:28:59 -0300

    resolucao da questao08

---
+++ src/questao08/Principal.java
@@ -0,0 +1,12 @@
+package questao08;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String [] args){
+        System.out.println("Números de (1 à 50)");
+
+        for (int i = 1; i <= 50; i++){
+            System.out.print(-i);
+        }
+    }
+}
